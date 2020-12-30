aa096c6fc2993e79c6ae27c7ab0083eb91b59508
wangyongbin <1105196302@qq.com>
2020-12-30 15:54:04 +0800

    12/30

--- espress-try/app.js
+++ espress-try/app.js
@@ -1,56 +1,56 @@
-const express = require('express')
-const app = express(); 
+const express = require("express");
+const app = express();
 const router = express.Router();
-const fn12 = new Promise((resolve) => {
-  console.log('resolve')
-  resolve();
-})
+// const fn12 = new Promise((resolve) => {
+//   console.log("resolve");
+//   resolve();
+// });
 // 完成跨域
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By", ' 3.2.1');
+  res.header("X-Powered-By", " 3.2.1");
   // res.header("Content-Type", "application/json;charset=utf-8");
-  console.log(Object.keys(res.sendDate),Object.keys(res.req.query))
+  console.log(Object.keys(res.sendDate), Object.keys(res.req.query));
   next();
-})
-app.use(async (req, res, next)=>{
-  console.log('app.1');
-  req = 1;
-  await next()
-  console.log('1');
-},async (req, res, next) => {
-  console.log('chain 1')
-  await next();
-  console.log('chain 1')
-})
-app.use( async (req, res, next)=>{
-  console.log('2');
-  fn12.then(() => {
-    console.log('reslove result')
-  })
-  await next();
-  console.log('2')
-})
-app.use(async (req, res, next)=>{
-  console.log(3)
-  await next()
-  console.log(3)
+});
+app.use(
+  (req, res, next) => {
+    console.log("app.1");
+    req = 1;
+    next();
+    console.log("1");
+  },
+  (req, res, next) => {
+    console.log("chain 1");
+    next();
+    console.log("chain 1");
+  }
+);
+app.use((req, res, next) => {
+  console.log("2");
+  next();
+  console.log("2");
+});
+app.use((req, res, next) => {
+  console.log(3);
+  next();
+  console.log(3);
   // res.send('welcome')
-})
-app.use(async (req, res, next)=>{
-  console.log('end')
-  await next()
-})
+});
+app.use((req, res, next) => {
+  console.log("end");
+  next();
+});
 // app.use((req, res, next) => {
 //   res.send("hello world i am the king of world")
 // })
-app.get('/', (req, res) => {
+app.get("/api/test", (req, res) => {
+  console.log("send");
   res.send("hello world, do your best");
-})
+});
 
-
-let server = app.listen(3001, () => {
+let server = app.listen(3300, () => {
   console.log("listen on port %d", server.address().port);
-})
+});
